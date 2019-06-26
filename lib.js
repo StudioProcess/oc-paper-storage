65e0111b15d3a687ad84f5069f302d19968cc352
Martin Leopold <m@martinleopold.com>
2019-06-26 12:06:20 +0200

    Image Generation (w/SVG support) as module

---
+++ lib.js
@@ -0,0 +1,79 @@
+const default_params = {
+  width: 1000,
+  sort: false,
+};
+
+// Encodes bytes (ArrayBuffer) into to an image (ImageData)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
+// https://developer.mozilla.org/en-US/docs/Web/API/ImageData
+export function encode(buf, params) {
+  
+  params = Object.assign({}, default_params, params);
+
+  let arr = new Uint8Array(buf); // make byte array
+  if (params.sort) arr.sort(); // sort values
+  
+  let w = params.width;
+  let h = Math.ceil( arr.length / w );
+  let img = new ImageData(w, h);
+  
+  console.log(`${arr.length} bytes`);
+  console.log(`${w}x${h} px`);
+  console.log(`${Math.ceil(w/300*25.4)} x ${Math.ceil(h/300*25.4)} mm @ 300 ppi`);
+  
+  for (let i=0; i<arr.length; i++) {
+    let g = arr[i]; // gray value
+    // console.log(g);
+    img.data[ i*4 + 0 ] = g;
+    img.data[ i*4 + 1 ] = g;
+    img.data[ i*4 + 2 ] = g;
+    img.data[ i*4 + 3 ] = 255;
+    
+    // img.data[i*4+0] = 255-g;
+    // img.data[i*4+1] = 255-g;
+    // img.data[i*4+2] = 255-g;
+    // img.data[i*4+3] = 255;
+    
+    // img.data[i*4+0] = 50;
+    // img.data[i*4+1] = 10;
+    // img.data[i*4+2] = 255-g;
+    // img.data[i*4+3] = 255;
+  }
+
+  return img;
+}
+
+export function img2svg(img) {
+  function makePixel(x, y, r, g, b, a) { return `<rect fill="rgba(${r},${g},${b},${a/255})" x="${x}" y="${y}" width="1" height="1" />`; }
+  let pixels = '';
+  for (let i=0; i<img.width*img.height; i++) {
+    pixels += makePixel(
+      i % img.width,
+      Math.ceil(i / img.width),
+      img.data[ i*4 + 0 ],
+      img.data[ i*4 + 1 ],
+      img.data[ i*4 + 2 ],
+      img.data[ i*4 + 3 ]
+    ) + '\n';
+  }
+  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${img.width} ${img.height}" shape-rendering="crispEdges">\n${pixels}</svg>`;
+}
+
+
+function saveURL(url, filename) {
+  let link = document.createElement('a');
+  link.download = filename;
+  link.href = url;
+  link.click();
+}
+
+function saveBlob(blob, filename) {
+  let url = URL.createObjectURL(blob);
+  saveURL(url, filename);
+  URL.revokeObjectURL(url);
+}
+
+export function saveSVG(string, filename) {
+  let blob = new Blob( [string], {type: 'image/svg+xml'} );
+  saveBlob(blob, filename);
+}
